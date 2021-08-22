a49aa5da7d60d94ab8e81e3eeb70a863b9d98cd8
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-08-22 15:17:02 +0200

    adding comments

--- src/QuadTree.hpp
+++ src/QuadTree.hpp
@@ -47,31 +47,29 @@ namespace QuadTree
 		int x1, y1, x2, y2;
 	};
 
-	// Represents an element node in the quadtree.
+	//! @brief Represents an element node in the quadtree.
 	struct QuadEltNode
 	{
-		// Points to the next element in the leaf node. A value of -1
-		// indicates the end of the list.
+		//! @brief Points to the next element in the leaf node. A value of -1 indicates the end of the list.
 		int next;
 
-		// Stores the element index.
+		//! @brief Stores the element index.
 		int element;
 	};
 
 	template<typename T>
 	class QuadTree
 	{
-		// Stores all the elements in the quadtree.
+		//! @brief Stores all the elements in the quadtree.
 		FreeList<std::shared_ptr<T>> elements;
 
-		// Stores all the element nodes in the quadtree.
-		FreeList<QuadEltNode> elt_nodes;
+		//! @brief Stores all the element nodes in the quadtree.
+		FreeList<QuadEltNode> elementNodes;
 
-		// Stores all the nodes in the quadtree. The first node in this
-		// sequence is always the root.
+		//! @brief Stores all the nodes in the quadtree. The first node in this sequence is always the root.
 		std::vector<QuadNode> nodes;
 
-		// Stores the quadtree extents.
+		//! @brief Stores the quadtree extents.
 		double xmin;
 		double ymin;
 		double xmax;
@@ -83,21 +81,29 @@ namespace QuadTree
 		// back of the nodes array.
 		int free_node = -1;
 
-		// Stores the maximum depth allowed for the quadtree.
-		unsigned int max_depth;
-		unsigned int max_element_per_node;
-
 		//! @brief Split a leaf node into children
 		//! @note rect [0] xmin [1] ymin [2] xmax [3] ymax
 		void split_leaf(QuadNode &leaf, const std::array<double, 4> &rect);
 
+		//! @brief creates the necessary QuadEltNode in the tree
 		void addElementQuadNodeInTree(int elementIndex, QuadNode &node, const std::array<double, 4> &rect,
 		                              unsigned int depth);
 
 	public:
 
+		//! @brief Stores the maximum depth allowed for the quadtree.
+		//! @note The tree won't be recreated when changing this value
+		unsigned int maxDepth;
+
+		//! @brief Stores the maximum number of elements allowed in a quadrant
+		//! @note The tree won't be recreated when changing this value
+		unsigned int maxElementsPerNode;
+
+		//! @brief Add an element in the tree
 		void add(std::shared_ptr<T> element);
 
+
+		//! @brief create a quadtree
 		explicit QuadTree(double x1, double y1, double x2, double y2);
 	};
 
@@ -108,8 +114,8 @@ namespace QuadTree
 		   ymin(y1),
 		   xmax(x2),
 		   ymax(y2),
-		   max_depth(5),
-		   max_element_per_node(8)
+		   maxDepth(5),
+		   maxElementsPerNode(8)
 	{
 	}
 
@@ -124,7 +130,7 @@ namespace QuadTree
 		const double childHeight = (rect[3] - rect[1]) / 2;
 
 		do {
-			auto &elementNode = this->elt_nodes[elementNodeIndex];
+			auto &elementNode = this->elementNodes[elementNodeIndex];
 			auto &element = this->elements[elementNode.element];
 
 			// top right
@@ -132,28 +138,28 @@ namespace QuadTree
 			                          rect[1],
 			                          rect[0] + childWidth,
 			                          rect[1] + childHeight})) {
-				indexes_to_link[0].emplace_back(this->elt_nodes.insert({-1, elementNode.element}));
+				indexes_to_link[0].emplace_back(this->elementNodes.insert({-1, elementNode.element}));
 			}
 			// top left
 			if (element->collideRect({rect[0] + childWidth,
 			                          rect[1],
 			                          rect[0] + childWidth + childWidth,
 			                          rect[1] + childHeight})) {
-				indexes_to_link[1].emplace_back(this->elt_nodes.insert({-1, elementNode.element}));
+				indexes_to_link[1].emplace_back(this->elementNodes.insert({-1, elementNode.element}));
 			}
 			// bottom right
 			if (element->collideRect({rect[0],
 			                          rect[1] + childHeight,
 			                          rect[0] + childWidth,
 			                          rect[1] + childHeight + childHeight})) {
-				indexes_to_link[2].emplace_back(this->elt_nodes.insert({-1, elementNode.element}));
+				indexes_to_link[2].emplace_back(this->elementNodes.insert({-1, elementNode.element}));
 			}
 			// bottom left
 			if (element->collideRect({rect[0] + childWidth,
 			                          rect[1] + childHeight,
 			                          rect[0] + childWidth + childWidth,
 			                          rect[1] + childHeight + childHeight})) {
-				indexes_to_link[3].emplace_back(this->elt_nodes.insert({-1, elementNode.element}));
+				indexes_to_link[3].emplace_back(this->elementNodes.insert({-1, elementNode.element}));
 			}
 
 			indexes_to_remove.emplace_back(elementNodeIndex);
@@ -163,12 +169,12 @@ namespace QuadTree
 		for (const auto &indexes : indexes_to_link) {
 			this->nodes.emplace_back(indexes.empty() ? -2 : indexes[0], indexes.size());
 			for (int i = 0; i < static_cast<int>(indexes.size()) - 1; i++) {
-				this->elt_nodes[indexes[i]].next = indexes[i + 1];
+				this->elementNodes[indexes[i]].next = indexes[i + 1];
 			}
 		}
 
 		for (const auto &index : indexes_to_remove) {
-			this->elt_nodes.erase(index);
+			this->elementNodes.erase(index);
 		}
 
 		leaf.firstChild = this->nodes.size() - 4;
@@ -245,21 +251,21 @@ namespace QuadTree
 		int quadNodeIndex = node.firstChild;
 
 		if (node.count == 0) {
-			node.firstChild = this->elt_nodes.insert({-1, elementIndex});
+			node.firstChild = this->elementNodes.insert({-1, elementIndex});
 			node.count++;
 			return;
 		} else {
 			while (true) {
-				if (this->elt_nodes[quadNodeIndex].next == -1) {
-					this->elt_nodes[quadNodeIndex].next = this->elt_nodes.insert({-1, elementIndex});
+				if (this->elementNodes[quadNodeIndex].next == -1) {
+					this->elementNodes[quadNodeIndex].next = this->elementNodes.insert({-1, elementIndex});
 					node.count++;
 					break;
 				}
-				quadNodeIndex = this->elt_nodes[quadNodeIndex].next;
+				quadNodeIndex = this->elementNodes[quadNodeIndex].next;
 			}
 		}
 
-		if (depth < this->max_depth && static_cast<unsigned>(node.count) > this->max_element_per_node) {
+		if (depth < this->maxDepth && static_cast<unsigned>(node.count) > this->maxElementsPerNode) {
 			this->split_leaf(node, rect);
 		}
 
